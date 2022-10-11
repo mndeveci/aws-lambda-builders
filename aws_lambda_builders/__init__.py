54bdd7eeb64cc9dfc271ec1dc23d753a7119fcd9
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-10-11 15:03:05 -0700

    chore: add some comments

--- aws_lambda_builders/__init__.py
+++ aws_lambda_builders/__init__.py
@@ -1,5 +1,10 @@
 """
 AWS Lambda Builder Library
 """
+
+# Changing version will trigger a new release!
+# Please make the version change as the last step of your development
 __version__ = "1.19.0"
+
+
 RPC_PROTOCOL_VERSION = "0.3"
