3019112aa3a836b24efbb19294f6fe7f42a09537
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-10-11 15:47:09 -0700

    chore: bump version to 1.19.1

--- aws_lambda_builders/__init__.py
+++ aws_lambda_builders/__init__.py
@@ -4,7 +4,7 @@ AWS Lambda Builder Library
 
 # Changing version will trigger a new release!
 # Please make the version change as the last step of your development.
-__version__ = "1.19.0"
+__version__ = "1.19.1"
 
 
 RPC_PROTOCOL_VERSION = "0.3"
