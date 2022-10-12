8ff38aeeb060ad66255db7855e64ff1057136856
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-10-11 23:36:47 -0700

    change version back to 19

--- aws_lambda_builders/__init__.py
+++ aws_lambda_builders/__init__.py
@@ -4,7 +4,7 @@ AWS Lambda Builder Library
 
 # Changing version will trigger a new release!
 # Please make the version change as the last step of your development.
-__version__ = "1.18.0"
+__version__ = "1.19.0"
 
 
 RPC_PROTOCOL_VERSION = "0.3"
